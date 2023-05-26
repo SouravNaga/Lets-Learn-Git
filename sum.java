0ee1693024e0a20cf93001f0c0f931aee4a6cb5b
Sourav Naga <souravnaga97@gmail.com>
2023-05-26 14:13:46 +0530

    modified sum.java

--- sum.java
+++ sum.java
@@ -1,9 +1,9 @@
-public static void main()
-{
+public class sum {
     public void main(String args[])
     {
         int a=4;
         int b=5;
         System.out.println(a+b);
+        System.out.println("test");
     }
 }
